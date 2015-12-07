a61b32529a1a50f767ea2f20588066bc832a3b91
alex-13021991 <alex13021991@gmail.com>
2015-12-07 12:24:55 +0600

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir -p /home/dell/asus_a501cg/build
-mkdir -p /home/dell/asus_a501cg/out
-rm /home/dell/asus_a501cg/build/bzImage
+mkdir -p ~/asus_a501cg/build
+mkdir -p ~/asus_a501cg/out
+rm ~/asus_a501cg/build/bzImage
 make -j5 -f KernelMakefile TARGET_DEVICE=a500cg build_bzImage
-find /home/dell/asus_a501cg/out/a500cg -type f -name bzImage -exec cp {} /home/dell/asus_a501cg/build/ \;
-rm -rf /home/dell/asus_a501cg/out/a500cg
+find ~/asus_a501cg/out/a500cg -type f -name bzImage -exec cp {} ~/asus_a501cg/build/ \;
+rm -rf ~/asus_a501cg/out/a500cg
