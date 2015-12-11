2451296966b83c5853994a6bf4432e43251a5754
alex-13021991 <alex13021991@gmail.com>
2015-12-11 14:10:27 +0600

    Add BioShock CPU Gov

--- linux/kernel/include/linux/cpufreq.h
+++ linux/kernel/include/linux/cpufreq.h
@@ -399,6 +399,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
 #endif
 
 
