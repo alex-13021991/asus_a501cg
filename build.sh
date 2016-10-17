547a9b7ea1c5e0a4d9a430c21641c09f935cccbd
alex-13021991 <alex13021991@gmail.com>
2016-10-17 20:44:50 +0600

    updading 'building process'

--- build.sh
+++ build.sh
@@ -3,32 +3,10 @@
 mkdir -p ~/asus_a501cg/build
 mkdir -p ~/asus_a501cg/out
 rm ~/asus_a501cg/build/bzImage
-rm -rf ~/asus_a501cg/build/lib
+rm -rf ~/asus_a501cg/build/modules
+mkdir -p ~/asus_a501cg/build/modules
+make -j5 -f KernelMakefile build_bzImage
 make -j5 -f KernelMakefile modules_install
-find ~/asus_a501cg/out/a500cg -type f -name bzImage -exec cp {} ~/asus_a501cg/build/ \;
-mkdir -p ~/asus_a501cg/build/lib
-mkdir -p ~/asus_a501cg/build/lib/modules
-find ~/asus_a501cg/out/a500cg -type f -name atomisp-css2300.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name bcm_bt_lpm.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name bcm43362.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name cfg80211.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name gc2155.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name hm2056.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name imx111.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name imx219.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name kxtj9.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name mac80211.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name pnwdisp.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name pvrsgx.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name rmi4.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name rt8515.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name st_drv.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name t4k35_180.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name t4k35.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name test_nx.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name videobuf-core.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name videobuf-vmalloc.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-find ~/asus_a501cg/out/a500cg -type f -name tcp_bic.ko -exec cp {} ~/asus_a501cg/build/lib/modules/ \;
-mkdir -p ~/asus_a501cg/build/lib/other_modules
-find ~/asus_a501cg/out/a500cg -type f -name *.ko -exec cp {} ~/asus_a501cg/build/lib/other_modules/ \;
+find ~/asus_a501cg/out/a500cg/linux/kernel/arch/x86/boot -type f -name bzImage -exec cp {} ~/asus_a501cg/build/ \;
+find ~/asus_a501cg/out/a500cg/linux/kernel/modules_install/lib/modules -type f -name *.ko -exec cp {} ~/asus_a501cg/build/modules/ \;
 rm -rf ~/asus_a501cg/out
